7a662384d99e86c3e10acf976b23dc6fa5dcf26d
Casey Heitz <heitz.casey@gmail.com>
2015-09-25 09:28:57 -0700

    Add StudentsController.js

---
+++ controllers/StudentsController.js
@@ -0,0 +1,8 @@
+studentCheckin.controller('StudentsCtrl', function StudentsCtrl($scope, StudentsFactory) {
+  $scope.students = StudentsFactory.students;
+  $scope.StudentsFactory = StudentsFactory;
+
+  $scope.checkin = function() {
+    $scope.checkinStatus = true;
+  };
+});

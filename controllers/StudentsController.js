287d3a085949fab9b063baa187d1e7b74dfa8ff9
Casey Heitz <heitz.casey@gmail.com>
2015-09-25 13:29:37 -0700

    Add comments

--- controllers/StudentsController.js
+++ controllers/StudentsController.js
@@ -2,6 +2,7 @@ studentCheckin.controller('StudentsCtrl', function StudentsCtrl($scope, Students
   $scope.students = StudentsFactory.students;
   $scope.StudentsFactory = StudentsFactory;
 
+//Toggles the student's checkinStatus between true and false
   $scope.toggleCheckin = function(student) {
     var index = $scope.students.indexOf(student);
     $scope.students[index].checkinStatus = !$scope.students[index].checkinStatus;
--- services/StudentsFactory.js
+++ services/StudentsFactory.js
@@ -1,8 +1,9 @@
 studentCheckin.factory('StudentsFactory', function StudentsFactory() {
   var factory = {};
   factory.students = [];
+//Adds a new student with an inputted name and a defaulted checkinStatus to false
   factory.addStudent = function() {
-    factory.students.push({name: factory.studentName, id: factory.students.length + 1, checkinStatus: false});
+    factory.students.push({name: factory.studentName, checkinStatus: false});
     factory.studentName = null;
   };
   return factory;
